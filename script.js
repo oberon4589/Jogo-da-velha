14a04690e75fd79b91e3a9949f3cc7868204e954
oberon4589 <oberon4589@gmail.com>
2023-07-04 15:19:08 -0300

    adicionando mudança de turno

--- script.js
+++ script.js
@@ -1,13 +1,50 @@
 const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.querySelector('[data-board]');
 
-for (const cell of cellElements) {
-    cell.addEventListener('click', handleClick, { once: true })
+let isCircleTurn = false;
+const startGame = () => {
+    for (const cell of cellElements) {
+        cell.addEventListener('click', handleClick, { once: true })
+    }
+
+    isCircleTurn = false;
+
+    board.classList.add('x');
 }
 
-const handleClick = () => {
-    //colocar a lógica do jogo aqui
+const placeMark = (cell, classToAdd) => {
+    cell.classList.add(classToAdd);
+}
 
+const swapTurns = () => {
+    isCircleTurn = !isCircleTurn;
+
+    board.classList.add('circle');
+    board.classList.remove('x');
+
+    if (isCircleTurn) {
+        board.classList.add('circle');
+    }else {
+        board.classList.add('x');
+    }
+}
+
+const handleClick = (e) => {
+    //colocar a lógica do jogo aqui
+    const cell = e.target;
+    const classToAdd = isCircleTurn ? 'circle' : 'x';
+
+    placeMark(cell, classToAdd);
     //checar por vitória
 
     //checar por empate
+
+    //Mudar simbolo
+    swapTurns();
 }
+
+for (const cell of cellElements) {
+    cell.addEventListener('click', handleClick, { once: true });
+}
+
+startGame();
